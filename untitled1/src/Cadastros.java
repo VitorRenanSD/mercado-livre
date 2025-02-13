1d71a74bf8fadfa129124095657104ad8d926bab
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-13 17:38:39 -0300

    Update Cadastros.java

--- untitled1/src/Cadastros.java
+++ untitled1/src/Cadastros.java
@@ -5,17 +5,17 @@ public class Cadastros {
     private List<String> usuarios = new ArrayList<>();
     private List<String> produtos = new ArrayList<>();
 
-    public void CadastraUser(){
+    public void CadastraUser(String usuario){
       usuarios.add(usuario);
         System.out.println(usuario);
     }
 
-    public void CadastroProduto(){
+    public void CadastroProduto(String cadastraproduto){
        produtos.add(cadastraproduto);
         System.out.println(cadastraproduto);
     }
 
-    public void RemoverProduto(){
+    public void RemoverProduto(String removeproduto){
         produtos.remove(removeproduto);
         System.out.println(removeproduto);
     }
