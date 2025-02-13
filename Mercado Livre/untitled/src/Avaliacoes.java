dd5161ca6a31301cae7ac6629e5763485f610fa9
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-13 15:53:20 -0300

    Delete Mercado Livre directory

--- Mercado Livre/untitled/src/Avaliacoes.java
+++
@@ -1,8 +0,0 @@
-public class Avaliacoes {
-    private int id;
-    private int id_product;
-    private int id_user;
-    private int rating;
-    private String text;
-
-}
--- Mercado Livre/untitled/src/Product.java
+++
@@ -1,9 +0,0 @@
-public class Product {
-    private int id;
-    private String product_name;
-    private String description;
-    private Float price;
-    private int quantity;
-    private String image_file;
-
-}
