88c1a23cf9a299f9bce5c2c2cc854c3f95a6d186
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-13 17:36:45 -0300

    Update Cadastros.java

--- untitled1/src/Cadastros.java
+++ untitled1/src/Cadastros.java
@@ -1,35 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Cadastros {
     private List<String> usuarios = new ArrayList<>();
     private List<String> produtos = new ArrayList<>();
 
     public void CadastraUser(){
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("digite o nome do usuário para cadastrar: ");
-        String usuario = leitor.nextLine();
-        usuarios.add(usuario);
+      usuarios.add(usuario);
         System.out.println(usuario);
     }
 
     public void CadastroProduto(){
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("digite o nome do produto para cadastrar: ");
-        String produto = leitor.nextLine();
-        produtos.add(produto);
-        System.out.println(produto);
-
+       produtos.add(cadastraproduto);
+        System.out.println(cadastraproduto);
     }
 
     public void RemoverProduto(){
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("digite o nome do produto a ser removido");
-        String produto = leitor.nextLine();
-        System.out.println(produto);
-        produtos.remove(produto);
-        System.out.println(produtos);
+        produtos.remove(removeproduto);
+        System.out.println(removeproduto);
     }
 }
 
