43b6414001a0de93af09774fc55e4a07ec14f6ae
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-13 17:37:41 -0300

    Update Consultas.java

--- untitled1/src/Consultas.java
+++ untitled1/src/Consultas.java
@@ -1,16 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Consultas {
 
     public void ListarVendas(){
-        List<String> vendas = new ArrayList<>();
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("digite o nome do produto para listar em vendas: ");
-        String venda = leitor.nextLine();
-        vendas.add(venda);
-        System.out.println(venda);
-
+             List<String> vendas = new ArrayList<>();
+        vendas.add(produto);
+        System.out.println(produto);
     }
 }
