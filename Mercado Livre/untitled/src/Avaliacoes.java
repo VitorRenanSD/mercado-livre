d8b3c9f2747f0ced6d267ce933878282dc8b4cff
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-13 13:56:30 -0300

    Add files via upload

---
+++ Mercado Livre/untitled/src/Avaliacoes.java
@@ -0,0 +1,8 @@
+public class Avaliacoes {
+    private int id;
+    private int id_product;
+    private int id_user;
+    private int rating;
+    private String text;
+
+}
---
+++ Mercado Livre/untitled/src/Sales.java
@@ -0,0 +1,9 @@
+public class Sales {
+    private int id;
+    private int product_id;
+    private int quantity;
+    private int id_payment_method;
+    private Boolean cancelled;
+    private String reason_cancel;
+}
+
