bac17a25ca66797af675e735c776af3f9fd67e25
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-13 17:39:50 -0300

    Update Main.java

--- untitled1/src/Main.java
+++ untitled1/src/Main.java
@@ -3,18 +3,16 @@ public class Main {
     public static void main(String[] args) {
 
         Cadastros usuario1 = new Cadastros();
-        usuario1.CadastraUser();
+        usuario1.CadastraUser("Douglas");
 
         Cadastros produto1 = new Cadastros();
-        produto1.CadastroProduto();
+        produto1.CadastroProduto("Banana");
 
         Consultas listar = new Consultas();
-        listar.ListarVendas();
+        listar.ListarVendas("Banana");
 
         Cadastros remover = new Cadastros();
-        remover.RemoverProduto();
-
-
-
+        remover.RemoverProduto("Banana");
     }
 }
+
