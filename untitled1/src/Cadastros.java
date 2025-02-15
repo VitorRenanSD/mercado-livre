0fa6ef4414cda5216e44be88dbc2008dee74b2dc
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-15 13:41:57 -0300

    Update Cadastros.java

--- untitled1/src/Cadastros.java
+++ untitled1/src/Cadastros.java
@@ -1,12 +1,14 @@
 public class Cadastro {
 
-
     public void AdicionaUser(){
-        String sql = "INSERT INTO cad_usuarios(username, cpf_cnpj, endereco, email, senha) VALUES  (?, ?, ?, ?, ?)";
-
+        String sql = "INSERT INTO cad_usuarios(username, cpf_cnpj, endereco, email, senha) VALUES  ("João Silva", "12345678901" , 
+            "Rua A 123" , "joao.silva@email.com", "senha123)";
     }
 
     public  void  AdicionarProduto(){
-        String sql = "INSERT INTO produtos(nome, descricao, preco_venda, estoque, caminho_imagem, usado) (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO produtos(nome, descricao, preco_venda, estoque, caminho_imagem, usado) ("Nootebok Deel", "Notebook Del Inspirion 15"  8GB RAMM 256 SSD,
+             "3.500,00", "imagens/notebook_dell.jpg", "False");
     }
+
+    
 }
