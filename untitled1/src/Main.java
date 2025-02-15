b26a23116a638235403671e2f8be2aac279bebf8
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-15 14:51:01 -0300

    Update Main.java

--- untitled1/src/Main.java
+++ untitled1/src/Main.java
@@ -1,18 +1,7 @@
-
 public class Main {
     public static void main(String[] args) {
 
-        Cadastros usuario1 = new Cadastros();
-        usuario1.CadastraUser("Douglas");
-
-        Cadastros produto1 = new Cadastros();
-        produto1.CadastroProduto("Banana");
-
-        Consultas listar = new Consultas();
-        listar.ListarVendas("Banana");
-
-        Cadastros remover = new Cadastros();
-        remover.RemoverProduto("Banana");
+        ConexaoDB conectar = new ConexaoDB();
+        conectar.connection();
     }
 }
-
