296db87f19c24624d9c9479aabeae1b866c962e4
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-15 13:46:46 -0300

    Update Cadastros.java

--- untitled1/src/Cadastros.java
+++ untitled1/src/Cadastros.java
@@ -6,7 +6,7 @@ public class Cadastro {
     }
 
     public void AicioanarProduto(){
- String sql = "INSERT INTO produtos(nome, descricao, preco_venda, estoque, caminho_imagem, usado)
+        String sql = "INSERT INTO produtos(nome, descricao, preco_venda, estoque, caminho_imagem, usado)
     VALUES ('Notebook Dell', 'Notebook Dell Inspiron 15 com 8GB RAM e 256GB SSD', '3500.00', 'imagens/notebook_dell.jpg', 'False')";
     }
 }
