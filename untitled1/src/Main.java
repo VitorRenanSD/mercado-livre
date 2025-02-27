7b0203df8498d6b640379236d22be04793d32f88
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-27 01:09:06 -0300

    Delete untitled1/src directory

--- untitled1/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        ConexaoDB conectar = new ConexaoDB();
-        conectar.connection();
-    }
-}
--- untitled1/src/Sales.java
+++
@@ -1,8 +0,0 @@
-public class Sales {
-    private int id;
-    private int product_id;
-    private int quantity;
-    private int id_payment_method;
-    private Boolean cancelled;
-    private String reason_cancel;
-}
